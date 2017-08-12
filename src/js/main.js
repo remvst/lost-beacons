07914961394068f7a1f26e0575c23425bfaa4e7f
remvst <remvst@gmail.com>
2017-08-12 15:07:43 -0400

    Initial framework for the game

---
+++ src/js/main.js
@@ -0,0 +1,19 @@
+onload = function() {
+    C = D.querySelector('canvas');
+    C.width = CANVAS_WIDTH;
+    C.height = CANVAS_HEIGHT;
+
+    R = C.getContext('2d');
+
+    // Shortcut for all canvas methods
+    var p = CanvasRenderingContext2D.prototype;
+    Object.getOwnPropertyNames(p).forEach(function(n) {
+        if (R[n] && R[n].call) {
+            w[n] = p[n].bind(R);
+        }
+    });
+
+    onresize();
+
+    // new Game();
+};
---
+++ src/js/util/globals.js
@@ -0,0 +1,6 @@
+var D = document,
+    w = window,
+    delayed = setTimeout,
+    C, // canvas
+    R, // canvas context
+    mobile = navigator.userAgent.match(nomangle(/andro|ipho|ipa|ipo|windows ph/i));
