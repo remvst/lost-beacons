87e96991022efc11934cb5c411dcdec89e413f30
remvst <remvst@gmail.com>
2017-08-20 10:29:08 -0400

    Saving bytes everywhere

--- src/js/util/perlin.js
+++ src/js/util/perlin.js
@@ -17,15 +17,13 @@
 class Grad {
 
     constructor(x, y, z) {
-        this.x = x; this.y = y; this.z = z;
+        this.x = x;
+        this.y = y;
+        this.z = z;
     }
 
     dot2(x, y) {
-        return this.x*x + this.y*y;
-    }
-
-    dot3(x, y, z) {
-        return this.x*x + this.y*y + this.z*z;
+        return this.x * x + this.y * y;
     }
 
 }
@@ -36,50 +34,22 @@ const grad3 = [
     new Grad(0,1,1),new Grad(0,-1,1),new Grad(0,1,-1),new Grad(0,-1,-1)
 ];
 
-var p = [
-    151,160,137,91,90,15,
-    131,13,201,95,96,53,194,233,7,225,140,36,103,30,69,142,8,99,37,240,21,10,23,
-    190, 6,148,247,120,234,75,0,26,197,62,94,252,219,203,117,35,11,32,57,177,33,
-    88,237,149,56,87,174,20,125,136,171,168, 68,175,74,165,71,134,139,48,27,166,
-    77,146,158,231,83,111,229,122,60,211,133,230,220,105,92,41,55,46,245,40,244,
-    102,143,54, 65,25,63,161, 1,216,80,73,209,76,132,187,208, 89,18,169,200,196,
-    135,130,116,188,159,86,164,100,109,198,173,186, 3,64,52,217,226,250,124,123,
-    5,202,38,147,118,126,255,82,85,212,207,206,59,227,47,16,58,17,182,189,28,42,
-    223,183,170,213,119,248,152, 2,44,154,163, 70,221,153,101,155,167, 43,172,9,
-    129,22,39,253, 19,98,108,110,79,113,224,232,178,185, 112,104,218,246,97,228,
-    251,34,242,193,238,210,144,12,191,179,162,241, 81,51,145,235,249,14,239,107,
-    49,192,214, 31,181,199,106,157,184, 84,204,176,115,121,50,45,127, 4,150,254,
-    138,236,205,93,222,114,67,29,24,72,243,141,128,195,78,66,215,61,156,180
-];
-
 // To remove the need for index wrapping, double the permutation table length
-var perm = new Array(512);
-var gradP = new Array(512);
+const perm = new Array(512);
+const gradP = new Array(512);
 
 // This isn't a very good seeding function, but it works ok. It supports 2^16
 // different seed values. Write something better if you need more seeds.
 function seed(seed) {
-    if(seed > 0 && seed < 1) {
-        // Scale the seed out
-        seed *= 65536;
+    const p = [];
+    for (let i = 0 ; i < 256 ; i++) {
+        p.push(~~(random() * 256));
     }
 
-    seed = floor(seed);
-    if(seed < 256) {
-        seed |= seed << 8;
-    }
-
-    for(let i = 0; i < 256; i++) {
-        let v;
-        if (i & 1) {
-            v = p[i] ^ (seed & 255);
-        } else {
-            v = p[i] ^ ((seed>>8) & 255);
-        }
-
+    p.forEach((v, i) => {
         perm[i] = perm[i + 256] = v;
         gradP[i] = gradP[i + 256] = grad3[v % 12];
-    }
+    });
 }
 
   // ##### Perlin noise stuff
@@ -95,7 +65,7 @@ function lerp(a, b, t) {
 // 2D Perlin Noise
 function perlin2(x, y) {
     // Find unit grid cell containing point
-    var X = floor(x), Y = floor(y);
+    const X = floor(x), Y = floor(y);
     // Get relative xy coordinates of point within that cell
     x = x - X; y = y - Y;
     // Wrap the integer cells at 255 (smaller integer period can be introduced here)
@@ -116,37 +86,4 @@ function perlin2(x, y) {
         lerp(n01, n11, u),
         fade(y)
     );
-}
-
-function yolo() {
-    seed(0);
-
-    const canvas = document.createElement('canvas');
-    canvas.width = GRID_COLS;
-    canvas.height = GRID_ROWS;
-
-    const ctx = canvas.getContext('2d');
-    const imageData = ctx.getImageData(0, 0, GRID_COLS, GRID_ROWS);
-
-    for (let row = 0 ; row < GRID_ROWS ; row++) {
-        for (let col = 0 ; col < GRID_COLS ; col++) {
-            const index = (row * GRID_ROWS + col) * 4;
-
-            let value = Math.abs(perlin2(row / 5, col / 5));
-            // value *= 256;
-            value = Math.pow(value, 2);
-
-            const threshold = 0.1;
-            value = value > threshold ? 0 : 255;
-
-            imageData.data[index] = value;
-            imageData.data[index + 1] = value;
-            imageData.data[index + 2] = value;
-            imageData.data[index + 3] = 255;
-        }
-    }
-
-    ctx.putImageData(imageData, 0, 0);
-
-    document.body.appendChild(canvas);
 }
